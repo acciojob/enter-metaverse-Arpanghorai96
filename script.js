b1956a07dd71c9bca34897221b26f10c1e48f1bb
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-04-06 06:54:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let enterBtn=document.getElementById("enterBtn");
+let status=document.getElementById("status");
+enterBtn.addEventListener("click",()=>{
+	status.innerText="Entered Metaverse";
+})
